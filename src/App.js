0e6c37368e2ddeb6578c7846f88456ec946c8ede
Marcos Moura <marcosmoura201@hotmail.com>
2023-01-01 11:34:04 -0300

    Comando para inserir dados na api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+
 const url = "http://localhost:3000/products"
 
 
 function App() {
 
-  
   const [products, setProducts] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
 
   // 1- Resgatando dados da api
    useEffect(() => {
@@ -24,7 +26,22 @@ function App() {
    }, []);
 
   // 2- adiconando dados na api
+   const handleSubmit = async (e) => {
+      e.preventDefault();
 
+      const product = {
+        name,
+        price,
+      }
+
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type" : "application/json",
+        },
+        body: JSON.stringify(product)
+      })
+   }
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
@@ -33,6 +50,20 @@ function App() {
           <li key={product.id}>{product.name} -R$: {product.price}</li>
         ))}
       </ul>
+      <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+            </label>
+
+            <label>
+              Preço:
+              <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+            </label>
+            <input type="submit" value="Enviar" />
+          </form>
+      </div>
     </div>
   );
 }
